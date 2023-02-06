a26eb25a866ebde5f549c34cce97ac863cda6589
itayhashay <itay0806@gmail.com>
2023-02-06 18:44:38 +0200

    start to create swagger

--- Backend/index.js
+++ Backend/index.js
@@ -7,12 +7,49 @@ const express = require('express'),
     dotenv = require('dotenv').config(),
     jwt = require('jsonwebtoken'),
     statusCode = require('http-status-codes').StatusCodes,
-    port = 8080;
+    port = 8080,
+    swaggerJSDoc = require('swagger-jsdoc'),
+    swaggerUi = require('swagger-ui-express');
 
 // CORS Handling    
 app.use(cors({
     origin: '*'
 }));
+
+// Swagger Setup
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Public API Swagger',
+      version: '1.0.0',
+      description: 'An api swagger',
+    },
+    tags: [
+        {
+            name: 'Category',
+            description: 'Operations related to categories',
+        },
+        {
+            name: 'User',
+            description: 'Operations related to users',
+        },
+        {
+            name: 'API',
+            description: 'Operations related to apis',
+        },
+        {
+            name: 'Bookmark',
+            description: 'Operations related to bookmarks',
+        },
+    ],
+  };
+  
+  const options = {
+    swaggerDefinition,
+    apis: ['./Controllers/*.js'],
+  };
+  const swaggerSpec = swaggerJSDoc(options);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Cookies and body parsers    
 app.use(cookieParser());
@@ -49,9 +86,9 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true })
 
 // Routes    
 app.use('/category',categoryRoute);
-app.use('/user', userRoute);
-app.use('/api', apiRoute);
-app.use('/bookmark', bookmarkRoute);
+app.use('/user',userRoute);
+app.use('/api',apiRoute);
+app.use('/bookmark', bookmarkRoute);  
 
 // Server Runner
 app.listen(port, () => {
