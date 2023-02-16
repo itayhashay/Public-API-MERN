5f63042721973ee8c97a084aaa0c83c86a577c9a
itay_daoss <itay.dauss@gmail.com>
2023-02-16 09:10:51 +0200

    add font Nunito

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,6 +5,7 @@ import App from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./fonts/Nunito/Nunito-Regular.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
