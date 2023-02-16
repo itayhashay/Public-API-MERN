918525a04010766147061c91bde7ed55dd3495d9
itay_daoss <itay.dauss@gmail.com>
2023-02-16 11:03:37 +0200

    remove uneccessary history

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -6,12 +6,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./fonts/Nunito/Nunito-Regular.ttf";
-import { history } from "./utils/history.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router history={history}>
+    <Router>
       <ToastContainer />
       <App />
     </Router>
